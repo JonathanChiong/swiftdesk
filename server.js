e0d02c2d0c3d301c5ff4d529cca07d017c04f6ff
Jonathan <jonathan.chiong@outlook.com>
2021-01-23 20:59:47 +0800

    fix heroku-postbuild

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ connection.once("open", () => {
 });
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
