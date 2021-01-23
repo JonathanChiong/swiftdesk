ba9bf0bf34f24dea4e1aab4e3f6c866d5363216d
Jonathan <jonathan.chiong@outlook.com>
2021-01-24 00:52:00 +0800

    fix heroku-postbuild

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ const connection = mongoose.connection;
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
-
 mongoose.connect(
   "mongodb+srv://yyanky:emotions@swiftdesk.fdp9b.mongodb.net/swiftdesk?retryWrites=true&w=majority",
   {
@@ -33,7 +29,7 @@ if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
 
   const path = require("path");
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
@@ -45,3 +41,7 @@ const transactions = require(".//routes/transactions");
 app.use("/api/products", products);
 app.use("/api/user/inventory", userInventory);
 app.use("/api/transactions", transactions);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
