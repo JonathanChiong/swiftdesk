26b534ba7ce2d3fd83b66d625461499f03a54da7
Jonathan <jonathan.chiong@outlook.com>
2021-01-23 21:08:38 +0800

    fix heroku-postbuild

--- client/src/redux/order/orderActions.js
+++ client/src/redux/order/orderActions.js
@@ -10,7 +10,7 @@ export const fetchOrder = () => {
   return (dispatch) => {
     dispatch(fetchOrderBegin());
     axios
-      .get("http://localhost:4500/transactions/")
+      .get("/transactions/")
       .then((orders) => dispatch(fetchOrderSuccess(orders.data)))
       .catch((error) => dispatch(fetchOrderError(error.message)));
   };
@@ -33,7 +33,7 @@ export const fetchOrderError = (error) => ({
 export const createOrder = (order) => {
   return (dispatch) => {
     axios
-      .post("http://localhost:4500/transactions/add", order)
+      .post("/transactions/add", order)
       .then((response) => {
         dispatch(createOrderSuccess(response.data));
       })
