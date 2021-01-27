bf0936a91b725e4f736a96f19171d1e18722a9f6
Jonathan <jonathan.chiong@outlook.com>
2021-01-27 19:51:15 +0800

    restore

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV == "production") {
 
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "client/build/index.html"));
+    res.send("HEY!!!!!");
   });
 }
 
