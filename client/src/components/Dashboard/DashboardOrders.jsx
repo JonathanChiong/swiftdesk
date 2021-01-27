d97b34f9c42d188f716c9092081a3942d697a06e
Jonathan <jonathan.chiong@outlook.com>
2021-01-27 16:37:53 +0800

    stocks now being deducted in order

--- client/src/components/Dashboard/DashboardOrders.jsx
+++ client/src/components/Dashboard/DashboardOrders.jsx
@@ -5,6 +5,20 @@ import { useSelector } from "react-redux";
 export const DashboardOrders = () => {
   const orders = useSelector((state) => state.orders);
 
+  const calculateOrderTotal = (order) => {
+    let result = 0;
+    for (var i = 0; i < order.length; i++) {
+      result += order[i].price * order[i].quantity;
+    }
+
+    return result;
+  };
+
+  const formatDate = (mongoDate) => {
+    var date = new Date(mongoDate);
+    return date.toDateString();
+  };
+
   return (
     <Fragment>
       <h2 className="dashboardh2">Order history</h2>
@@ -23,30 +37,42 @@ export const DashboardOrders = () => {
           <Accordion defaultActiveKey="0">
             {orders.orders.map((order) => {
               return (
-                <Card key={order._id}>
+                <Card key={"00" + order._id}>
                   <Accordion.Toggle
                     as={Card.Header}
                     eventKey={order.invoice_num}
                   >
                     <span style={{ float: "right" }}>
-                      <strong>Total: Php {order.subTotal}</strong>
+                      Total:{" "}
+                      <strong>
+                        Php
+                        {calculateOrderTotal(order.order)}
+                      </strong>
                     </span>
-                    <span>
-                      <em>Invoice: {order.invoice_num}</em>
-                    </span>
+                    <span>Invoice: {order.invoice_num}</span>
                   </Accordion.Toggle>
                   <Accordion.Collapse eventKey={order.invoice_num}>
                     <Card.Body>
+                      <strong>Date: </strong>
+                      {formatDate(order.createdAt)}
+                      <br />
                       <strong>Name: </strong>
                       {` ${order.f_name} ${order.l_name}`}
                       <br />
                       <strong>Items:</strong>
                       <br />
-                      {`${order.quantity} ${order.order}`}
+                      {order.order.map((order, i) => {
+                        return (
+                          <div key={i + "orderlist"}>
+                            {`${order.quantity} ${order.product}`} - SRP{" "}
+                            {order.price}
+                          </div>
+                        );
+                      })}
                       <br />
-                      <strong>Subtotal:</strong>
+                      <strong>Total: </strong>
+                      {calculateOrderTotal(order.order)}
                       <br />
-                      {`Php ${order.subTotal}`}
                     </Card.Body>
                   </Accordion.Collapse>
                 </Card>
